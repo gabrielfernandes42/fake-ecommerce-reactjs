33f60313233cc04df354d3d01a042f77fef4e06b
gabriel <gabrielfernandes254@gmail.com>
2023-02-11 14:18:53 -0300

    product page done

--- client/src/pages/Product/Product.jsx
+++ client/src/pages/Product/Product.jsx
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import BalanceIcon from "@mui/icons-material/Balance";
 import "./Product.scss";
 
 function Product() {
@@ -23,20 +26,41 @@ function Product() {
       </div>
       <div className="right">
         <h2>Title</h2>
-        <span>$199</span>
+        <span className="price">$199</span>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
           assumenda provident deserunt, quos quaerat magni placeat dicta illum
           pariatur dolores fuga debitis necessitatibus fugit modi, atque
           veritatis expedita nesciunt excepturi?
         </p>
+
+        <div className="quantity">
+          <button
+            onClick={() => setQuantity((prev) => (prev === 1 ? 1 : prev - 1))}
+          >
+            -
+          </button>
+          {quantity}
+          <button onClick={() => setQuantity((prev) => prev + 1)}>+</button>
+        </div>
+        <button className="add">
+          <AddShoppingCartIcon /> Add to Cart
+        </button>
+        <div className="links">
+          <div className="item">
+            <FavoriteBorderIcon /> Add to wish list
+          </div>
+          <div className="item">
+            <BalanceIcon /> add to compare
+          </div>
+        </div>
+        <div className="info">
+          <span>Vendor: Polo</span>
+          <span>Product Type: T-shirt</span>
+          <span>Tag: T-shirt , woman, top</span>
+          <span></span>
+        </div>
       </div>
-      <div className="quantity">
-        <button onClick={() => setQuantity((prev) => (prev === 1 ? 1 : prev - 1))}>-</button>
-        {quantity}
-        <button onClick={() => setQuantity((prev) => (prev +1))}>+</button>
-      </div>
-      <button className=""></button>
     </div>
   );
 }
